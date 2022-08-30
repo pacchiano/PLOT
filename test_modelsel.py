2bba6170cb93821d1cffb92f899b829837e03445
Aldo Pacchiano <aldopacchiano@gmail.com>
2022-08-30 09:06:28 -0400

    changes to the code

--- test_modelsel.py
+++ test_modelsel.py
@@ -152,7 +152,7 @@ PLOT_MAHALANOBIS = True
 
 
 ## What is the fractrion of rejected labels for each algorithm and each dataset. 
-dataset = "Bank"# "Adult" ## "Adult", "Bank", "German"
+dataset = "German"# "Adult" ## "Adult", "Bank", "German"
 num_batches = 20
 epsilon = .1
 alpha = 10
