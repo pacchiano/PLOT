4e1e156d61fac24cd18e4510fb50c3807e852d6b
Aldo Pacchiano <aldopacchiano@gmail.com>
2022-11-19 12:47:28 -0500

    asdflkm

--- run_clip.sh
+++ run_clip.sh
@@ -1,16 +1,21 @@
 
 
-python clip_experiments .2 1000 False;
-python clip_experiments .3 1000 False; 
-python clip_experiments .5 1000 False; 
-python clip_experiments .7 1000 False; 
-python clip_experiments .8 1000 False;
+python clip_experiments.py .2 1000 False;
+python clip_experiments.py .3 1000 False; 
+python clip_experiments.py .5 1000 False; 
+python clip_experiments.py .7 1000 False; 
+python clip_experiments.py .8 1000 False;
 
 
 
-python clip_experiments .2 2000 False;
-python clip_experiments .3 2000 False; 
-python clip_experiments .5 2000 False; 
-python clip_experiments .7 2000 False; 
-python clip_experiments .8 2000 False;
+python clip_experiments.py .2 2000 False;
+python clip_experiments.py .3 2000 False; 
+python clip_experiments.py .5 2000 False; 
+python clip_experiments.py .7 2000 False; 
+python clip_experiments.py .8 2000 False;
 
+python clip_experiments.py .2 1000 True;
+python clip_experiments.py .3 1000 True; 
+python clip_experiments.py .5 1000 True; 
+python clip_experiments.py .7 1000 True; 
+python clip_experiments.py .8 1000 True;
