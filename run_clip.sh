145811c58b1fc8d05a92cce82dba008b82253465
Aldo Pacchiano <aldopacchiano@gmail.com>
2022-11-20 08:38:26 -0500

    asdflkm

--- run_clip.sh
+++ run_clip.sh
@@ -1,21 +1,21 @@
 
 
 python clip_experiments.py .2 1000 False;
-python clip_experiments.py .3 1000 False; 
-python clip_experiments.py .5 1000 False; 
-python clip_experiments.py .7 1000 False; 
-python clip_experiments.py .8 1000 False;
+# python clip_experiments.py .3 1000 False; 
+# python clip_experiments.py .5 1000 False; 
+# python clip_experiments.py .7 1000 False; 
+# python clip_experiments.py .8 1000 False;
 
 
 
-python clip_experiments.py .2 2000 False;
-python clip_experiments.py .3 2000 False; 
-python clip_experiments.py .5 2000 False; 
-python clip_experiments.py .7 2000 False; 
-python clip_experiments.py .8 2000 False;
+# python clip_experiments.py .2 2000 False;
+# python clip_experiments.py .3 2000 False; 
+# python clip_experiments.py .5 2000 False; 
+# python clip_experiments.py .7 2000 False; 
+# python clip_experiments.py .8 2000 False;
 
-python clip_experiments.py .2 1000 True;
-python clip_experiments.py .3 1000 True; 
-python clip_experiments.py .5 1000 True; 
-python clip_experiments.py .7 1000 True; 
-python clip_experiments.py .8 1000 True;
+# python clip_experiments.py .2 1000 True;
+# python clip_experiments.py .3 1000 True; 
+# python clip_experiments.py .5 1000 True; 
+# python clip_experiments.py .7 1000 True; 
+# python clip_experiments.py .8 1000 True;
